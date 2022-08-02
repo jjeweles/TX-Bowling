622e185d6b36188847b669979276bb757da2c6fb
Justin Jewell <77253839+jjeweles@users.noreply.github.com>
2022-08-02 17:44:59 -0500

    refactor city routes, moved finding tournament into function, cleaner code

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -1,26 +1,30 @@
 const Tourney = require("../models/tournaments.js");
 
+let getTournament = async (city) => {
+    return Tourney.find({tournamentLocation: city}).sort({tournamentDate: 1});
+}
+
 module.exports.getAustin = async (req, res) => {
-    let tournaments = await Tourney.find({tournamentLocation: "austin"});
+    let tournaments = await getTournament("austin")
     res.render('cities/austin', {tournaments});
 }
 
 module.exports.getDallas = async (req, res) => {
-    let tournaments = await Tourney.find({tournamentLocation: "dallas"});
+    let tournaments = await getTournament("dallas")
     res.render('cities/dallas', {tournaments});
 }
 
 module.exports.getHouston = async (req, res) => {
-    let tournaments = await Tourney.find({tournamentLocation: "houston"});
+    let tournaments = await getTournament("houston")
     res.render('cities/houston', {tournaments});
 }
 
 module.exports.getSanAntonio = async (req, res) => {
-    let tournaments = await Tourney.find({tournamentLocation: "sanantonio"});
+    let tournaments = await getTournament("sanantonio")
     res.render('cities/sanantonio', {tournaments});
 }
 
 module.exports.getOther = async (req, res) => {
-    let tournaments = await Tourney.find({tournamentLocation: "other"});
+    let tournaments = await getTournament("other")
     res.render('cities/other', {tournaments});
 }
